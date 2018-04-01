994c756fb83eed552c745d3a03215361cb1001ce
Nytheneals <nythes@gmail.com>
2018-04-01 18:33:08 -0400

    Initiated Server file using Express

---
+++ server.js
@@ -0,0 +1,27 @@
+// import express from "express";
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
+// SCHEMA IMPORT
+const schema = require("./schema");
+
+// INITIALIZING SERVER
+const app = express();
+
+// GRAPHQLHTTP FUNCTION FROM EXPRESS-GRAPHQL IS MIDDLEWARE
+// THAT HELPS EXPRESS UNDERSATND AND INTERACT WITH GRAPHQL.
+// WE SET IT UP AS MIDDLEWARE, SO WHEN A REQUEST IS MADE TO /GRAPHQL ,
+// THE MIDDLEWARE TAKES ON THE HEAVY WORK OF INTERPRETING GRAPHQL
+// IT TAKES IN AN OBJECT WITH THE SCHEMA OF THE GRAPH OF OUR DATA.
+
+//********GRAPH QL SERVER********//
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema
+  })
+);
+// LISTENING TO OUR SERVER FOR ANY CHANGES OR REQUESTS
+app.listen(3000, () => {
+  console.log("Now listening for request on port 3000");
+});
